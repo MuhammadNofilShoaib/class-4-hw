044a0cdfdcc48b81fff815bf25847a5578230461
Muhammad Nofil Shoaib <164288852+MuhammadNofilShoaib@users.noreply.github.com>
2024-10-10 18:30:33 +0500

    Add files via upload

---
+++ (public)/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "../globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default function PublicLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode; 
+}>) {
+  return (
+    <html lang="en">
+      <body
+      > 
+      <Header/>
+        {children}
+        <Footer/>
+      </body>
+    </html>
+  );
+}
